2b9abef9a5677498e3d7e484bced5831797e66f9
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-02-27 17:11:30 +0300

    "css module"

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('hello webpack');
+import { setItem } from './utils/storage.js';
+import './index.css';
+
+document.getElementById('btnSayHello').addEventListener('click', () => {
+    alert('hello webpack');
+
+    setItem('token', 'dfdsjhfjg34y732y4bjhsdb');
+});
